6c40318b40f75f3b8e68745da46452c86ab5560b
TJ Holowaychuk <tj@vision-media.ca>
2012-08-15 14:26:40 -0700

    fix aliasing

--- test/fixtures/alias.js
+++ test/fixtures/alias.js
@@ -3,8 +3,8 @@ require.register('foo/index.js', function(module, exports, require){
   module.exports = require('bar');
 });
 
-require.register('bar', function(module, exports, require){
+require.register('bar/index.js', function(module, exports, require){
   module.exports = 'bar';
 });
 
-require.alias('bar', 'foo/deps/bar');
+require.alias('bar/index.js', 'foo/deps/bar/index.js');
