ba6174fc3ed6b01af22cbde5525b070d404b1d93
Stephen Mathieson <smath23@gmail.com>
2013-09-07 22:13:25 -0400

    add support for exporting falsey values

---
+++ test/fixtures/falsey.js
@@ -0,0 +1,8 @@
+
+require.register('false', function(exports, require, module){
+  module.exports = false;
+});
+
+require.register('zero', function(exports, require, module){
+  module.exports = 0;
+});
